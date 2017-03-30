063a3a0321247bc0d90b69c99c907772d2103623
Andrej Stajer <adostajo@gmail.com>
2017-03-30 12:44:08 +0200

    attendance test int

---
+++ TrackingSystem/TrackingSystem.Tests/Controllers/AttendanceControllerTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TrackingSystem.Models;
+
+namespace TrackingSystem.Tests.Controllers
+{
+    [TestClass]
+    public class AttendanceControllerTest
+    {
+    }
+}

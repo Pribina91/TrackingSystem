9359c3f6d21f10207b7526dff9fced34d175bfb1
Andrej Stajer <adostajo@gmail.com>
2017-03-30 10:01:12 +0200

    db init

---
+++ TrackingSystem/TrackingSystem.Database/Tables/dbo/Attendance.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Attendance]
+(
+    [AttendanceId] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [CardId] INT NOT NULL, 
+    [CheckIn] DATETIME2 NOT NULL, 
+    [CheckOut] DATETIME2 NULL, 
+    CONSTRAINT [FK_Attendance_VisitorCard] FOREIGN KEY ([CardId]) REFERENCES [dbo].[VisitorCard]([CardId])
+)
---
+++ TrackingSystem/TrackingSystem.Database/Tables/dbo/VisitorCard.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[VisitorCard]
+(
+    [CardId] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [Active] BIT NOT NULL, 
+    [VisitorName] NVARCHAR(MAX) NOT NULL
+)
